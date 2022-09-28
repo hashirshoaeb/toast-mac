a6f7df3c25187ce64c5c546892a7e67b830bbecc
hashirshoaeb <hashirshoaeb@gmail.com>
2022-09-28 07:27:16 +0500

    updates

--- install.sh
+++ install.sh
@@ -1,33 +1,5 @@
 echo "Welcome Hashir!"
-echo "It's time to configure your new mac."
-echo "Sit back sir, grab a coffee and relax, I got this!!"
-
-echo "Setting up your Mac..."
-
-# Setting mac defaults
-# To read the properties you can automate, just read the plist using app's idenfitier
-# i.e.
-# defaults read com.apple.finder
-# reads the properties for finder app, and then you can write the once you wish to change
-# https://github.com/mathiasbynens/dotfiles/blob/main/.macos
-
-echo "Setting up mac defaults of your choice"
-echo "- Finder: show hidden files by default"
-defaults write com.apple.finder AppleShowAllFiles -bool true
-echo "- Dock: move dock to the left side of screen"
-defaults write com.apple.dock orientation left
-echo "- Screencapture: Save screenshots to the desktop"
-defaults write com.apple.screencapture location -string "$HOME/Desktop"
-echo "- Screencapture: Save screenshots in PNG format"
-defaults write com.apple.screencapture type -string "png"
-echo "- Terminal: setup terminal flavour"
-# opening hsterm automatically imports it in terminal preferences.
-open ~/toast-mac/hsterm.terminal          
-defaults write com.apple.terminal "Startup Window Settings" -string "hsterm" 
-defaults write com.apple.terminal "Default Window Settings" -string "hsterm" 
-# Disable the sound effects on boot
-# sudo nvram SystemAudioVolume=" "  
-echo "mac defaults updated."
+echo "It's time to setup your new mac."
 
 
 echo "Checking for Oh My Zsh and install if we don't have it"
@@ -35,20 +7,6 @@ if test ! $(which omz); then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     echo "OMZ is installed"
 fi
-
-# `ln` creates symbolic links. 
-# https://man7.org/linux/man-pages/man1/ln.1.html
-# Symlinks tend to like full paths
-# -s make symbolic links instead of hard links
-# -f remove existing destination files
-# -v print name of each linked file
-
-echo "Installing your personal config dotfiles"
-
-ln -sfv ~/toast-mac/dotfiles/.gitconfig ~
-ln -sfv ~/toast-mac/dotfiles/.gitignore_global ~
-ln -sfv ~/toast-mac/dotfiles/.zshrc ~
-
 
 echo "Check for Homebrew and install if we don't have it"
 which -s brew
@@ -61,9 +19,31 @@ fi
 
 # brews 
 brew install gh
+# vs code 
+brew install --cask visual-studio-code
+# android studio
+brew install --cask android-studio
+
 
 # todo
 # install node, flutter vscode, xcode, xcode commandline tools, android studio 
 # arc browser 
+
+# Flutter
+# https://docs.flutter.dev/get-started/install/macos
+# https://stackoverflow.com/questions/65259300/detect-apple-silicon-from-command-line
+
+if [[ $(arch) == 'arm64' ]]; then
+    sudo softwareupdate --install-rosetta --agree-to-license
+    echo M1
+fi
+
+git clone https://github.com/flutter/flutter.git -b stable
+which flutter
+sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+sudo xcodebuild -runFirstLaunch
+sudo xcodebuild -license
+
+
 
 echo "Finished"
---
+++ sync.sh
@@ -0,0 +1,43 @@
+echo "Welcome Hashir!"
+echo "It's time to sync your mac."
+echo "Sit back sir, grab a coffee and relax, I got this!!"
+
+echo "Setting up your Mac..."
+
+# Setting mac defaults
+# To read the properties you can automate, just read the plist using app's idenfitier
+# i.e.
+# defaults read com.apple.finder
+# reads the properties for finder app, and then you can write the once you wish to change
+# https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+
+echo "Setting up mac defaults of your choice"
+echo "- Finder: show hidden files by default"
+defaults write com.apple.finder AppleShowAllFiles -bool true
+echo "- Dock: move dock to the left side of screen"
+defaults write com.apple.dock orientation left
+echo "- Screencapture: Save screenshots to the desktop"
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+echo "- Screencapture: Save screenshots in PNG format"
+defaults write com.apple.screencapture type -string "png"
+echo "- Terminal: setup terminal flavour"
+# opening hsterm automatically imports it in terminal preferences.
+open ~/toast-mac/hsterm.terminal          
+defaults write com.apple.terminal "Startup Window Settings" -string "hsterm" 
+defaults write com.apple.terminal "Default Window Settings" -string "hsterm" 
+# Disable the sound effects on boot
+# sudo nvram SystemAudioVolume=" "  
+echo "mac defaults updated."
+
+# `ln` creates symbolic links. 
+# https://man7.org/linux/man-pages/man1/ln.1.html
+# Symlinks tend to like full paths
+# -s make symbolic links instead of hard links
+# -f remove existing destination files
+# -v print name of each linked file
+
+echo "Installing your personal config dotfiles"
+
+ln -sfv ~/toast-mac/dotfiles/.gitconfig ~
+ln -sfv ~/toast-mac/dotfiles/.gitignore_global ~
+ln -sfv ~/toast-mac/dotfiles/.zshrc ~
